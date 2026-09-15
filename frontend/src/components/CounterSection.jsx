79f272cd5f73a625b2480ed6b2838465c6c48f22
muhafiat15 <muhafiat15@gmail.com>
2026-09-15 11:12:59 +0800

    pesan perubahan

--- frontend/src/components/CounterSection.jsx
+++ frontend/src/components/CounterSection.jsx
@@ -42,7 +42,7 @@ export default function CounterSection() {
   ];
 
   return (
-    <section className="py-16 bg-lars-navy relative overflow-hidden border-t border-white/10">
+    <section className="pt-8 pb-24 md:pb-32 bg-lars-navy relative overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-12 text-center">
           {stats.map((stat, index) => (
@@ -64,6 +64,13 @@ export default function CounterSection() {
             </motion.div>
           ))}
         </div>
+      </div>
+      
+      {/* Wave Transition to next section (White) */}
+      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none rotate-180 text-white z-0 -mb-[1px]">
+        <svg className="relative block w-[calc(100%+1.3px)] h-[40px] md:h-[80px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="fill-current"></path>
+        </svg>
       </div>
     </section>
   );
--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -86,8 +86,8 @@ export default function HeroSection() {
         </div>
       </motion.div>
 
-      {/* Organic Wave Transition to next section (White) */}
-      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none rotate-180 text-white z-10 -mb-[1px]">
+      {/* Organic Wave Transition to next section (Navy) */}
+      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none rotate-180 text-lars-navy z-10 -mb-[1px]">
         <svg className="relative block w-[calc(100%+1.3px)] h-[40px] md:h-[80px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="fill-current"></path>
         </svg>
